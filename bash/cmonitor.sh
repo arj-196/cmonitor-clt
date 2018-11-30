c3065ac4532df97c033861d6d64ae791b4f6dbfa
arj-196 <arjruler93@gmail.com>
2018-11-30 19:38:51 +0100

    integrate with api

--- bash/cmonitor.sh
+++ bash/cmonitor.sh
@@ -6,28 +6,42 @@ command=$2
 hostname="http://localhost:3000/api"
 wait_interval=1
 
+PY_PARSE_JSON="python -c \"import sys, json; print json.load(sys.stdin)['"
+PY_PARSE_JSON_END="']\""
+
 echo "--------------------------------"
 echo "---    Monitoring Command    ---"
 echo "--------------------------------"
 echo "-> Label      : ${label}"
 echo "-> Command    : ${command}"
 
+#read -r -p "Continue? [y/N] " response
+#case "$response" in
+#    [yY][eE][sS]|[yY])
+#        break
+#        ;;
+#    *)
+#        echo "--- Exiting ---"
+#        exit 1
+#        ;;
+#esac
 
 submit_start () {
-    echo "curl POST ${hostname}/task --label=\"${label}\" --command=\"${command}\""
-    # TODO retrieve task id
-    task="some task id"
-    echo "curl POST ${hostname}/task/instance --task=\"${task}\""
-    instance="some instance id"
+    task=$(curl -s -d "label=${label}&command=${command}" -X POST "${hostname}/task" \
+                | python -c "import sys, json; print json.load(sys.stdin)['id']")
+    # for kill any active instances
+    curl -s -d "task=${task}" -X POST "${hostname}/task/instance/kill"
+    instance=$(curl -s -d "task=${task}" -X POST "${hostname}/task/instance" \
+                | python -c "import sys, json; print json.load(sys.stdin)['id']")
 }
 
 submit_end () {
-    echo "curl DELETE ${hostname}/task/instance --task=\"${task}\" --instance=\"${instance}\""
+    curl -s -d "task=${task}&instance=${instance}" -X DELETE "${hostname}/task/instance"
 }
 
-submit_running () {
-    echo "curl UPDATE ${hostname}/task/instance --task=\"${task}\" --instance=\"${instance}\""
-}
+#submit_running () {
+#    echo "curl -d \"task=${task}&instance=${instance}\" -X UPDATE ${hostname}/task/instance"
+#}
 
 #
 # launch command
@@ -60,25 +74,9 @@ do
     process_count
     echo -ne "-> Run time   : ${var} seconds\r"
     sleep ${wait_interval}
-    submit_running
+    # TODO integrate logic of gathering run data, like logs
+#    submit_running
 done
 
-echo "-> Run time   : ${var} seconds"
-
-
-
-
-
-
-#echo -ne '#####                     (33%)\r'
-#sleep 1
-#echo -ne '#############             (66%)\r'
-#sleep 1
-#echo -ne '#######################   (100%)\r'
-#echo -ne '\n'
-
-#
-#ps aux | grep "${command}"
-
-
 submit_end
+echo "-> Run time   : ${var} seconds"
